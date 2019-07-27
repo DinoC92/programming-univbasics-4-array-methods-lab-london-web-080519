6c4dd980a34b294aef3fb96938886935b4620804
Dean Burgess <dinocastiglione@ymail.com>
2019-07-27 22:16:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,5 @@
 def using_include(array, element)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
-def using_include(array, element)
-  array.include?(element)
+  array.include?("wow")
 end
 
 def using_sort(array)
